d2ab36c5c5c20a1b41dbbc56793d6046b0ae0bf6
Jeff Lincoln <lincolnpitch493@gmail.com>
2024-01-04 10:21:01 +0300

    FinalChanges4

--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -1,7 +1,7 @@
 import { Project } from '../typings';
 
 export const fetchProjects = async() => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProject`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/getProject`)
 
     const data = await res.json();
     const projects: Project[] = data.projects;
